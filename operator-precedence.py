f37f236d8e334d7ea4861bc7794b84b545902e46
Taylor Lee <taylor.dennis.lee@gmail.com>
2024-12-03 10:01:41 -0600

    Added operator precedence

---
+++ operator-precedence.py
@@ -0,0 +1,21 @@
+# Operator Precedence
+
+# Python's Operator Precedence follows the PEMDAS acronym
+# Parenthesis
+# Exponentiation
+# Multiplication
+# Division
+# Addition
+# Subtraction
+
+# Let's look at an example.
+
+result = 2**3 // 2 * 3 + 5 - 1
+print(result)
+
+# Here we get 2^3 = 8
+# 8 // 2 = 4
+# 4 * 3 = 12
+# 12 + 5 = 17
+# 17 - 1 = 16
+# And when we run the script and print result, we get output of 16.
